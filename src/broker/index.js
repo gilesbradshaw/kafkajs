048b224f3f91fbaeea1320ee5b7762aa275ce8cb
Giles Bradshaw <giles.bradshaw@sigyl.com>
2019-11-05 18:38:47 +0000

    ..

--- src/broker/index.js
+++ src/broker/index.js
@@ -70,8 +70,6 @@ module.exports = class Broker {
    * @returns {Promise}
    */
   async connect() {
-    await this.lock.acquire()
-
     if (this.isConnected()) {
       return
     }
@@ -110,7 +108,6 @@ module.exports = class Broker {
       await authenticator.authenticate()
       this.authenticated = true
     }
-    await this.lock.release()
   }
 
   /**
